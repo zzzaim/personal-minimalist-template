26597b27b2f4bc092330c753112cc7ea46d32f02
Zaim Bakar <37629946+zzzaim@users.noreply.github.com>
2020-02-16 16:53:26 +0800

    Add pug-lint config

---
+++ .pug-lintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  disallowAttributeInterpolation: true,
+  disallowClassAttributeWithStaticValue: true,
+  disallowBlockExpansion: true,
+  disallowDuplicateAttributes: true,
+  disallowIdAttributeWithStaticValue: true,
+  disallowLegacyMixinCall: true,
+  requireClassLiteralsBeforeAttributes: true,
+  requireIdLiteralsBeforeAttributes: true
+};
