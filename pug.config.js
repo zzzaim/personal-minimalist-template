fd647cca4f07e2cb966aea98b823c4e257f3da56
Zaim Bakar <37629946+zzzaim@users.noreply.github.com>
2020-02-16 20:34:05 +0800

    Update design and src directory organisation

--- pug.config.js
+++ pug.config.js
@@ -4,15 +4,29 @@ module.exports = {
     subtitle: "Blockchain Developer",
 
     tips: {
+      // Change this to your .crypto domain
       address: "nakamoto.crypto",
-      acceptedCoins: ["BTC", "ETH", "LTC", "XRP"]
+
+      // Change these to all the currencies you accept
+      coins: ["BTC", "ETH", "LTC", "XRP"]
     },
 
+    // Change these to your own social media links
     links: [
       { site: "twitter", name: "satoshi" },
       { site: "github", name: "satoshi" },
       { site: "medium", name: "satoshi" },
-      { site: "facebook", name: "SatoshiNakamoto" }
-    ]
+      { site: "facebook", name: "SatoshiNakamoto" },
+      { site: "youtube", name: "SatoshiNakamoto" }
+    ],
+
+    // Change the following to your own referral code,
+    // or to `null` if you don't have one or don't
+    // want to use this template author's code.
+    unstoppableDomains: "50b92404bd8449b"
+
+    // NOTE: To get a referral code, register an account
+    // at unstoppabledomains.com and go to this page:
+    // https://unstoppabledomains.com/refer-a-friend
   }
 };
